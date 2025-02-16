4811abc212e852dd65e419691a6b32f1be866fab
kimjong69 <sahilbhosale700@gmail.com>
2025-02-15 23:41:45 -0500

    commit-1

--- expr.c
+++ expr.c
@@ -3,6 +3,7 @@
 #include "stdio.h"
 
 
+
 struct Expr *mk_plus(struct Expr *e1, struct Expr *e2) {
     struct Expr *ret = malloc(sizeof(struct Expr));
     ret->type = PLUS;
@@ -52,6 +53,14 @@ struct Expr *mk_float(float f) {
  */
 struct Expr *mk_expr1() {
     /* TODO: Your code here */
+    struct Expr *e1 = mk_float(1);
+    struct Expr *e2 = mk_float(4);
+    struct Expr *e3 = mk_float(5);
+
+    struct Expr *mul = mk_times(e2, e3);
+    struct Expr *add = mk_plus(e1, mul);
+
+    return add;
 }
 
 /* This function should create the expr (1 + (7 / 8))
@@ -59,6 +68,14 @@ struct Expr *mk_expr1() {
  */
 struct Expr *mk_expr2() {
     /* TODO: Your code here */
+    struct Expr *e1 = mk_float(1);
+    struct Expr *e2 = mk_float(7);
+    struct Expr *e3 = mk_float(8);
+
+    struct Expr *div = mk_div(e2, e3);
+    struct Expr *add = mk_plus(e1,div);
+
+    return add;
 }
 
 /* This function should create the expr ((1 / 3) - (4 / (2 + 3)))
@@ -66,6 +83,17 @@ struct Expr *mk_expr2() {
  */
 struct Expr *mk_expr3() {
     /* TODO: Your code here */
+    struct Expr *e1 = mk_float(1);
+    struct Expr *e2 = mk_float(3);
+    struct Expr *e3 = mk_float(4);
+    struct Expr *e4 = mk_float(2);
+
+    struct Expr *div1 = mk_div(e1,e2);
+    struct Expr *add = mk_plus(e4,e2);
+    struct Expr *div2 = mk_div(e3,add);
+    struct Expr *sub = mk_minus(div1,div2);
+
+    return sub;
 }
 
 
@@ -75,6 +103,9 @@ struct Expr *mk_expr3() {
 */
 void free_expr(struct Expr* e) {
     /* TODO: Your code here */
+    free_expr(e->subexprs.e1);
+    free_expr(e->subexprs.e2);
+    free(e);
 }
 
 /*
@@ -83,6 +114,21 @@ void free_expr(struct Expr* e) {
 */
 float eval(struct Expr* e) {
     /* TODO: Your code here */
+    if(e->type==FLOAT){
+        return e->literal;
+    }
+    else if(e->type==PLUS){
+        return eval(e->subexprs.e1) + eval(e->subexprs.e2);
+    }
+    else if(e->type==MINUS){
+        return eval(e->subexprs.e1) - eval(e->subexprs.e2);
+    }
+    else if(e->type==TIMES){
+        return eval(e->subexprs.e1) * eval(e->subexprs.e2);
+    }
+    else if(e->type==DIV){
+        return eval(e->subexprs.e1) / eval(e->subexprs.e2);
+    }
 }
 
 
